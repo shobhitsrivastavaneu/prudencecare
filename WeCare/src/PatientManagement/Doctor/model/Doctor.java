1058cdcf028b18d788adbea4214b22e0eb5947e1
Shobhit Srivastava <srivastava.sho@northeastern.edu>
2022-11-29 21:30:16 -0500

    Adding doctor model

--- WeCare/src/PatientManagement/Doctor/model/Doctor.java
+++ WeCare/src/PatientManagement/Doctor/model/Doctor.java
@@ -4,10 +4,94 @@
  */
 package PatientManagement.Doctor.model;
 
+
 /**
  *
- * @author rishabagarwal
+ * @author shobhitsrivastava
  */
 public class Doctor {
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+    
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+    String address;
+    String city;
+    String state;
+    String department;
+    int departmentId;
     
 }
