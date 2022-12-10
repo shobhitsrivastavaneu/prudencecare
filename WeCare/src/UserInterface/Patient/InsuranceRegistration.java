68f037d847edcec754d249b6eca0622d22647cb7
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2022-12-09 20:02:28 -0500

    adding patient insurance registration files

--- WeCare/src/UserInterface/Patient/InsuranceRegistration.java
+++ WeCare/src/UserInterface/Patient/InsuranceRegistration.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UserInterface.Patient;
 
 import Business.EcoSystem;
@@ -41,7 +37,7 @@ import userinterface.InsuranceAdminWorkArea.ViewPolicyWorkAreaJPanel;
 
 /**
  *
- * @author Anjali
+ * @author rishabagarwal
  */
 public class InsuranceRegistration extends javax.swing.JPanel {
 
