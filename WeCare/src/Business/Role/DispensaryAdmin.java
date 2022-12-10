deef31505f14c7172312a2f3ccf037d3b7a6d791
pillai-p <pillai.p@northeastern.edu>
2022-12-10 17:33:16 -0500

    Adding dispensary admin

---
+++ WeCare/src/Business/Role/DispensaryAdmin.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Enterprise.DispensaryEnterprise;
+import Business.Organization.LaboratoryOrganization;
+import Business.Organization.Organization;
+import Business.Organization.DispensaryOrganization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.LabStaffRole.LabStaffWorkAreaJPanel;
+import userinterface.PharmaAdmin.PharmacyAdminPageJPanel;
+
+/**
+ *
+ * @author parvathypillai
+ */
+public class DispensaryAdmin extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new PharmacyAdminPageJPanel(userProcessContainer, account,(DispensaryOrganization)organization,(DispensaryEnterprise) enterprise,business);
+    }
+
+    public String toString(){
+        return "PharmacyAdmin";
+    }  
+    
+}
