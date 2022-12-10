83e59f2ec50b8dbde5bbb09da76375cc62db830f
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2022-12-09 20:58:13 -0500

    adding some patient files

--- WeCare/src/UserInterface/Patient/HospitalAppointment.java
+++ WeCare/src/UserInterface/Patient/HospitalAppointment.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UserInterface.Patient;
 
 import Business.EcoSystem;
@@ -26,7 +22,7 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author sayu
+ * @author rishabagarwal
  */
 public class HospitalAppointment extends javax.swing.JPanel {
 
